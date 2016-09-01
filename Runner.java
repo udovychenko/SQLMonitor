fc64aaf71b5fa43e2bc75564975d58bab052eab5
udovychenko <udaff79@gmail.com>
2016-09-01 22:33:34 +0300

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,12 @@
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        CheckHost c = new CheckHost();
+        String host = "google.com";
+
+        System.out.println(c.printResult(host));
+
+    }
+}
